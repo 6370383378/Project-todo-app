a33cb7b3fd5f254a0b189ef054c0dcb1009288a2
JITENDRA PRASAD BARIK <103168577+6370383378@users.noreply.github.com>
2023-04-16 14:46:02 +0530

    Add files via upload

---
+++ TodoContainer.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import TodoItem from "./TodoItem";
+import { Bars } from "react-loader-spinner";
+
+function TodoContainer({ addedTask }) {
+	const [todos, setTodos] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [newTask, setNewTask] = useState(addedTask);
+	// console.log(addedTask);
+
+
+	// Whenever the new task is added then that task
+	useEffect(() => {
+		let taskAddedByUser = addedTask.map((task, index) => {
+			return <TodoItem task={task} key={index} />;
+		});
+		setNewTask(taskAddedByUser)
+	},  [newTask])
+
+	// console.log(todos);
+	// let todoItemsHtml = ;
+	// console.log(todoItemsHtml);
+	return (
+		<div className="todo-container">
+			{newTask}
+			{loading ? (
+				todos.map((task, index) => {
+					return <TodoItem task={task} key={index} />;
+				})
+			) : (
+				<Bars height="80" width="80" color="#4fa94d" ariaLabel="bars-loading" visible={true} />
+			)}
+		</div>
+	);
+}
+
+export default TodoContainer;
